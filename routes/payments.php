a8815054690e50193cf136c7e472861f5aca4498
Joe Gitonga <joseph.gitonga@afrinettelecom.co.ke>
2020-11-26 12:28:05 +0300

    cb2: cannot register urls due to daraja api downtime

--- routes/payments.php
+++ routes/payments.php
@@ -4,12 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('access_token', [App\Http\Controllers\Api\MpesaController::class, 'generateAccessToken'])
+Route::post('access_token', [\App\Http\Controllers\Api\MpesaController::class, 'generateAccessToken'])
     ->name('payments.access_token');
-Route::post('stk_push', [App\Http\Controllers\Api\MpesaController::class, 'customerSTKPush'])
-    ->name('payments.stk_push');
-Route::post('c2b_validation', [App\Http\Controllers\Api\MpesaController::class, 'c2bValidation'])
+Route::post('stk_init', [\App\Http\Controllers\Api\MpesaController::class, 'stkInit'])
+    ->name('payments.stk_init');
+Route::post('stk_save', [\App\Http\Controllers\Api\MpesaController::class, 'stkSave'])
+    ->name('payments.stk_save');
+Route::post('c2b_validation', [\App\Http\Controllers\Api\MpesaController::class, 'c2bValidation'])
     ->name('payments.c2b_validation');
-Route::post('c2b_confirmation', [App\Http\Controllers\Api\MpesaController::class, 'c2bConfirmation'])
+Route::post('c2b_confirmation', [\App\Http\Controllers\Api\MpesaController::class, 'c2bConfirmation'])
     ->name('payments.c2b_confirmation');
+Route::post('c2b_register_urls', [\App\Http\Controllers\Api\MpesaController::class, 'c2bRegisterUrls'])
+    ->name('payments.c2b_register_urls');
+Route::post('b2c_init', [\App\Http\Controllers\Api\MpesaController::class, 'b2cInit'])
+    ->name('payments.b2c_init');
+Route::post('b2c_save', [\App\Http\Controllers\Api\MpesaController::class, 'b2cSave'])
+    ->name('payments.b2c_save');
 
